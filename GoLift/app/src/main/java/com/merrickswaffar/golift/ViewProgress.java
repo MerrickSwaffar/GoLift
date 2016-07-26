2b406d20644bb8a4b9a1512736be123814ec7a66
Merrick Swaffar <mwswaffa@ucsc.edu>
2016-07-26 15:59:14 -0700

    date selector fixed

--- GoLift/app/src/main/java/com/merrickswaffar/golift/ViewProgress.java
+++ GoLift/app/src/main/java/com/merrickswaffar/golift/ViewProgress.java
@@ -13,6 +13,7 @@ import java.util.Date;
 public class ViewProgress extends AppCompatActivity {
 
     CalendarView calendar;
+    String selectedDate;
     Button viewExercises, viewMeals;
 
     @Override
@@ -20,7 +21,19 @@ public class ViewProgress extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_progress);
 
+        selectedDate = User.getDate();
+
         calendar = (CalendarView) findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                String m = month + "";
+                if (m.length() < 2) m = "0" + m;
+                String day = dayOfMonth + "";
+                if (day.length() < 2) day = "0" + day;
+                selectedDate = m + "/" + day +"/" + year;
+            }
+        });
 
         viewMeals = (Button) findViewById(R.id.bMeals);
         viewExercises = (Button) findViewById(R.id.bExercises);
@@ -28,17 +41,17 @@ public class ViewProgress extends AppCompatActivity {
         View.OnClickListener onClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-                String date = df.format(new Date(calendar.getDate()));
+                //SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+                //String date = df.format(new Date(calendar.getDate()));
                 switch (v.getId()){
                     case R.id.bMeals:
                         Intent i = new Intent(getApplicationContext(), ShowMeals.class);
-                        i.putExtra("date", date);
+                        i.putExtra("date", selectedDate);
                         startActivity(i);
                         break;
                     case R.id.bExercises:
                         Intent j = new Intent(getApplicationContext(), ShowExercises.class);
-                        j.putExtra("date", date);
+                        j.putExtra("date", selectedDate);
                         startActivity(j);
                 }
             }
--- GoLift/app/src/main/java/com/merrickswaffar/golift/Workout.java
+++
@@ -1,7 +0,0 @@
-package com.merrickswaffar.golift;
-
-/**
- * Created by Merrick on 7/12/2016.
- */
-public class Workout {
-}
